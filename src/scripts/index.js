455269b376df56eb4827e6a69a7e28e4eb99f0d7
Anthony <anthonywlynn2002@gmail.com>
2025-02-15 18:49:04 -0500

    feat: add array sort by geo distance and find user geo position

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,10 @@
 import "../pages/index.css";
+import getSortedArrayByGeoDistance from "../utils/getSortedArrayByGeoDistance.js";
+import restaurantsData from "../utils/restaurants.json";
+
+navigator.geolocation.getCurrentPosition((position) => {
+  const restaurants = getSortedArrayByGeoDistance(
+    restaurantsData.restaurants,
+    position.coords
+  );
+});
