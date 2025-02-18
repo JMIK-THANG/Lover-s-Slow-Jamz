b889adf0983c2125b1873f9699781650daf197ff
Jmik Thang <jmikthang5@gmail.com>
2025-02-18 07:03:27 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "/Lover-s-Slow-Jamz/",
+    publicPath: "/",
   },
 
   mode: "development",
